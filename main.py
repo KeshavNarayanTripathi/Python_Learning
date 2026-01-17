31300d251cc85edd647fab21031d66e1da7884a3
Keshav Tripathi <yourgithubemail@gmail.com>
2026-01-17 21:26:52 +0530

    Initial Python project commit

---
+++ main.py
@@ -0,0 +1,81 @@
+               # This is Our Fist Program
+print("Namaste VS Code , we are learning Python")
+
+
+                   # COMMENTS 
+# fOR SINGLE line comment we use Hastag(#) & Simply do (ctrl+/)
+'''for multiline comment 
+we can do single or double quoted 
+three times'''
+      # OR
+"""for multiline comment 
+we can do single or double quoted 
+three times"""  #this is also used for doc string
+
+
+                  # Variables 
+a = 10; 
+sher = "Keshav"   #we can not do 2sher means number phele nhi use krne 
+# no use space between variables like (SHE R), dont use special character like @sher
+SheryiansSchool = "students" #pascal case
+sheryiansSchool = "students" #camel  case
+sheryians_school = "students" #snake case
+
+                 #DataType
+#1 Numbers 
+a =10;      # Integer 
+b =20.5;    #float
+c =20j;     #complex where we use iota
+
+#2 Strings
+
+st = 'bshuxbhsbxhsbyg7s67as88876'; 
+am = "hbusudc8uwed723ye23ueuhd"; 
+
+#3 Boolean 
+# gives answer in true or false
+
+x = True; 
+y = False; 
+
+
+#String indexing
+
+she = "Romania"; 
+print(she[5]); #ans is 'i'
+print(she[-2]) #ans is 'i'
+
+
+#string slicing
+
+kes = "Keshav Tripathi"
+print(kes[0 : 10 : 2]);  # isme btata hai ki [start : end : kitni chhodh chhodh ke likhe] main thing end point nhi aata
+
+
+#TYPE Conversion
+
+a = 12;
+print(type(a))
+
+a = str(a)
+print(type(a))
+
+print(bool(a))  #Every value change in boolean and its will be in TRUE Except 7Values -> False, 0, 0.0, "", [List], (Tuple), {Dict}
+
+#implicit jisme python khud se ek dattype se dusre me change krta hai like-> 12/3 answer will be 4.0 so integer to string
+#Explicit jisme user use kare function
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
